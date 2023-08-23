6d8ba243ee6b0f42e678f209897a8a6afe7510f1
chanyu1 <haneul1ij@gmail.com>
2023-08-23 18:51:24 +0900

    a

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -60,7 +60,7 @@ export default function ContactForm() {
           onChange={onChange}
         />
         <label className="font-semibold text-white" htmlFor="subject">
-          Subject test d
+          Subject test dc
         </label>
         <input
           type="text"
